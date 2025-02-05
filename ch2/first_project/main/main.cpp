1285b3cba145d5d47c5a6ac152b3664eab3c8f07
Adam Seskunas <adamseskunas@gmail.com>
2025-02-05 10:22:54 -0800

    Delete uneeded files

--- ch2/first_project/main/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-extern "C" void app_main(void)
-{
-    std::cout << "Hello world!\n";
-}
--- ch2/platformio_ex/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-extern "C" void app_main()
-{
-    std::cout << "Hello World!\n";
-}
